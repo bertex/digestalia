3bf9f1c85ab13bc81ae9fabc1dff4800e76fdf26
Juanjo <juanjoinfoa>
2015-10-26 15:54:09 +0100

    Deleted some usless files updated WelcomTrail

--- js/cookies.js
+++
@@ -1,7 +0,0 @@
-window.cookieconsent_options = {
-	"message" : "Aquest pàgina web utilitza cookies per assegurar d'obtenir la millor experiència mentre visites la nostra web",
-	"dismiss" : "Accepta !!",
-	"learnMore" : "Més informació",
-	"link" : "cookies.html",
-	"theme" : "dark-bottom"
-};
